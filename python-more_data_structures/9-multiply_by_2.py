e13ce42bc46b6c06e985666ee5994cec0c80e179
Te1vel <c.ngabo@alustudent.com>
2026-07-10 22:59:27 +0200

    fixing qn 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if not isinstance(a_dictionary, dict):
-        return {}
-    return {key: value * 2 for key, value in a_dictionary.items()}
+    new_dictionary = {}
+    for key, value in a_dictionary.items():
+        new_dictionary[key] = value * 2
+    return new_dictionary
