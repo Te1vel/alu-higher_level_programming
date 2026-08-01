fed57ba9a4f81297eb4f88b93f5e5605310ae390
Te1vel <c.ngabo@alustudent.com>
2026-08-01 23:38:05 +0200

    correction on qn4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-documenting strings
-"""
+"""documenting strings"""
 import requests
 
-
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    if url.startswith('https://'):
-        url = "https://alu-intranet.hbtn.io/status"
+    url = 'https://alu-intranet.hbtn.io/status'
     res = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
