f17b1d30d1487df42f6a7ff9c92c24e516d0b1b1
Te1vel <c.ngabo@alustudent.com>
2026-08-01 23:32:44 +0200

    adding misisng file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+"""This is the State module.
+Contains the State class that inherits from Base = declarative_base()
 """
-Contains the class definition of a City
-"""
-from model_state import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-class City(Base):
+
+class State(Base):
+    """This class links to the `states` table of our database.
+    Attributes:
+        id (int): id of the state.
+        name (str): name of the state.
     """
-    Class that defines each city
-    """
-    __tablename__ = 'cities'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
